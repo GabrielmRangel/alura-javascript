44cea86ab44f54e3c9d9e61a6a37acf107da601f
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2022-11-10 16:39:12 -0300

    Curso 3 Aula 3

--- curso3/introducao-javascript/js/principal.js
+++ curso3/introducao-javascript/js/principal.js
@@ -1,32 +1,33 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Meus clientes";
 
-var paulo = document.querySelector('#paciente-paulo');
-var tdPauloPeso = paulo.querySelector('.info-peso');
-var pauloPeso = tdPauloPeso.textContent;
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPauloAltura = paulo.querySelector('.info-altura');
-var pauloAltura = tdPauloAltura.textContent;
+for (var i = 0; i <= pacientes.length; i++){
+    var tdPeso = pacientes[i].querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    var tdAltura = pacientes[i].querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    var tdImc = pacientes[i].querySelector('.info-imc');
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-var tdPauloImc = paulo.querySelector('.info-imc');
-
-var alturaEhValida = true;
-var pesoEhValido = true;
-
-if (pauloPeso <= 0 || pauloPeso >= 1000) {
-    console.log("Peso inválido!");
-    tdPauloImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        tdImc.textContent = "Peso inválido!";
+        pacientes[i].classList.add('paciente-invalido');
+        pesoEhValido = false;
+    }
+    
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida!");
+        tdImc.textContent = "Altura inválida!";
+        pacientes[i].classList.add('paciente-invalido');
+        alturaEhValida = false;
+    }
+    
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = Math.round(imc);    
+    }
 }
-
-if (pauloAltura <= 0 || pauloAltura >= 3) {
-    console.log("Altura inválida!");
-    tdPauloImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
-
-if (alturaEhValida && pesoEhValido) {
-    var pauloImc = pauloPeso / (pauloAltura * pauloAltura);
-    tdPauloImc.textContent = pauloImc;    
-}
-
